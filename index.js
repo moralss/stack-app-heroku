3f6648889b6b54ececb5fa900a686ec314167ea8
moral <moral@devson.co.za>
2021-01-05 12:55:43 +0200

    added code to package.json

--- index.js
+++ index.js
@@ -7,14 +7,26 @@ const
 const app = express()
 
 
-if (process.env.NODE_ENV === "production") {
-  //   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.use(express.static('client/dist/spa'));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist/spa", "index.html"))
-  })
-}
+// if (process.env.NODE_ENV === "production") {
+//   //   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.use(express.static('client/dist/spa'));
+//   app.get("*", (req, res) => {
+//     try {
+//       return res.sendFile(path.join(__dirname, "client", "dist/spa", "index.html"))
+//     } catch (e) {
+//       console.log(e)
+//       return 
+//     }
+//   })
+// }
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/dist/spa'));
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/dist/spa/index.html'));
+}
 
 // app.use(history())
 // app.use(serveStatic(__dirname + 'client/dist/spa'))
