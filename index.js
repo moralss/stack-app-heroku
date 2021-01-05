54d16acd5c146cabd7f2fe5589ea63d05e66925f
moral <moral@devson.co.za>
2021-01-05 11:28:40 +0200

    added deployment code

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 
 const express = require('express')
-//const serveStatic = require('serve-static')
-//const history = require('connect-history-api-fallback')
 const port = 5000
 
 const app = express()
 
-if (process.env.NODE_ENV === "production") {
-  //   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.use(express.static('client/dist/build/spa'));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
-  })
-}
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
 
-// app.use(history())
-// app.use(serveStatic(__dirname + '/dist/spa'))
-// app.listen(3000)
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
