8062c3427b9b4b74d27f50bc99b10ec62a235643
moral <moral@devson.co.za>
2021-01-05 11:38:08 +0200

    same

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const app = express()
 // Handle production
 if (process.env.NODE_ENV === 'production') {
   // Static folder
-  app.use(express.static(__dirname + '/public/'));
+  app.use(express.static(__dirname + 'client/public/'));
 
   // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + 'client/public/index.html'));
 }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
