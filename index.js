8b2812b9abeb551e7c09200d2bed535d5c3c137d
moral <moral@devson.co.za>
2021-01-05 10:23:01 +0200

    changed port

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
 const express = require('express')
-const serveStatic = require('serve-static')
-const history = require('connect-history-api-fallback')
+//const serveStatic = require('serve-static')
+//const history = require('connect-history-api-fallback')
 const port = 5000
 
 const app = express()
 
+if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/dist/build/spa'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+  })
+}
+
 // app.use(history())
-app.use(serveStatic(__dirname + '/dist/spa'))
-app.listen(3000)
+// app.use(serveStatic(__dirname + '/dist/spa'))
+// app.listen(3000)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
