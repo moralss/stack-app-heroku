2b5cc707ab547e128dd1d503d3e19e4ae6f0a749
moral <moral@devson.co.za>
2021-01-05 09:46:21 +0200

    added new env

--- index.js
+++ index.js
@@ -12,8 +12,17 @@ app.get('/', decodeIDToken, async (req, res) => {
     return
   }
 
+
   return res.status(403).send('Not authorized');
 });
+
+if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+  })
+}
 
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
