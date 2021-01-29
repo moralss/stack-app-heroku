dedd1c112a9aab484796f3d61edeeb81cf74fa8b
Moral <moral@devson.co.za>
2021-01-29 09:29:07 +0200

    added route for testing

--- index.js
+++ index.js
@@ -5,41 +5,20 @@ const
   port = process.env.PORT || 5000
 
 const app = express()
+const path = require('path');
+const { appRoutes } = require("./routes/appRoutes");
 
-// if (process.env.NODE_ENV === "production") {
-//   //   app.use(express.static(path.join(__dirname, 'client/build')));
-//   app.use(express.static('client/dist/spa'));
-//   app.get("*", (req, res) => {
-//     try {
-//       return res.sendFile(path.join(__dirname, "client", "dist/spa", "index.html"))
-//     } catch (e) {
-//       console.log(e)
-//       return 
-//     }
-//   })
-// }
+appRoutes(app)
+if (process.env.NODE_ENV === "production") {
 
-// Handle production
-if (process.env.NODE_ENV === 'production') {
-  // Static folder
-  // app.use(express.static(__dirname + '/client/dist/spa'));
-  // Handle SPA
-  // app.get(/.*/, (req, res) => {
-  //   return res.sendFile(__dirname + '/client/dist/spa/index.html')
-  // })
 
-  app.use(express.static('client/dist/spa'));
+  app.use(express.static(__dirname + 'client/dist/spa'));
   app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "client", "dist/spa", "index.html"))
-    // res.sendFile(__dirname + '/client/dist/spa/index.html')
   })
 }
 
-app.get("/api/car", (req, res) => {
-  res.send("hello world")
-})
-// app.use(history())
-// app.use(serveStatic(__dirname + 'client/dist/spa'))
+
 app.listen(port, () => {
   console.log("server running")
 })
